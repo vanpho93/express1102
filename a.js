f37f163130b0c4d15b073ebda0bd3cafb9b83fe8
Admin <vanpho02@gmail.com>
2017-02-19 11:16:17 +0700

    exports

---
+++ a.js
@@ -0,0 +1,6 @@
+console.log('Xin chao');
+
+let add = (a, b) => {
+  return a + b;
+}
+module.exports = add;
---
+++ b.js
@@ -0,0 +1,3 @@
+let a = require('./a.js');
+// console.log('a = ', a);
+console.log(a(10, 10));
